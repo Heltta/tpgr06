af676a216051830106101a1ba561837d17d66203
Heltta <lucashelgueraz@gmail.com>
2023-08-21 19:38:08 -0300

    Implement Usuario attributes

--- ServidorCentral/src/Logica/Usuario.java
+++ ServidorCentral/src/Logica/Usuario.java
@@ -1,5 +1,34 @@
 package Logica;
 
 public class Usuario {
+	private String nickname;
+	private String mail;
+	private String nombre;
+	private String apellido;
 
+	public String getNickname() {
+		return nickname;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setNickname(String newNick) {
+		this.nickname = newNick;
+	}
+	public void setMail(String newMail) {
+		this.nickname = newMail;
+	}
+	public void setNombre(String newNombre) {
+		this.nickname = newNombre;
+	}
+	public void setApellido(String newApellido) {
+		this.nickname = newApellido;
+	}
 }
