ca1e566aa13358cfca49b26dcee2c9d353e131a1
Bruno Romero <brunnorag@gmail.com>
2023-08-23 23:57:04 -0300

    consultaUsuario fix

--- ServidorCentral/src/Logica/CtrlUsuario.java
+++ ServidorCentral/src/Logica/CtrlUsuario.java
@@ -1,5 +1,18 @@
 package Logica;
 
-public class CtrlUsuario implements IUsuario {
 
+import java.util.ArrayList;
+
+public class CtrlUsuario implements IUsuario{
+	
+	public ArrayList<String> listarUsuarios(){
+		ManejadorUsuario manejador=ManejadorUsuario.getInstance();
+		return manejador.getNicknames();
+	}
+	public DTUsuario mostrarDatosUsuario(String nickname) {
+		ManejadorUsuario manejador=ManejadorUsuario.getInstance();
+		Usuario u= manejador.getUsuario(nickname);
+		DTUsuario datosUsuario= new DTUsuario(u.getNickname(), u.getMail(), u.getNombre(), u.getApellido());
+		return datosUsuario;
+	}
 }
--- ServidorCentral/src/Logica/ManejadorUsuario.java
+++ ServidorCentral/src/Logica/ManejadorUsuario.java
@@ -1,5 +1,40 @@
 package Logica;
 
-public class ManejadorUsuario {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+
+public class ManejadorUsuario {
+	
+	Map<String,Usuario> usuarios;
+	private static ManejadorUsuario instance;
+	
+	public static ManejadorUsuario getInstance() {
+		if(instance==null) {
+			instance=new ManejadorUsuario();
+		}
+		return instance;
+	}
+	
+	public ManejadorUsuario() {
+		usuarios= new HashMap<String,Usuario>();
+	}
+	
+	public ArrayList<String> getNicknames(){
+		ArrayList<String> nicknames = new ArrayList<String>();
+		usuarios.forEach((nickname, usuario)->{
+			nicknames.add(nickname);
+			} 
+		);
+		return nicknames;
+	}
+	public Usuario getUsuario(String nickname) {
+		
+		Usuario u=null;
+		if(usuarios.containsKey(nickname)) {
+			u=usuarios.get(nickname);
+		}
+		return u;
+	}
 }
