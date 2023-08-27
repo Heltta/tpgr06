7de701fa828a9b66387a1c0d99cd4d1c65ff643f
Flavio Cubon <flavio.cubon@fing.edu.uy>
2023-08-27 00:43:56 -0300

    Consulta paquete GUI y crear paquete

--- ServidorCentral/src/Presentacion/main.java
+++ ServidorCentral/src/Presentacion/main.java
@@ -39,6 +39,7 @@ public class main {
     private AgregarTipoPublicacionAPaquete frmAgregarTipoPublicacionAPaquete;
     private ConsultaOferta frmConsultaOferta;
     private CrearPaqueteTiposPublicacion frmCrearPaqueteTiposPublicacion;
+    private ConsultaPaquete frmConsultaPaquete;
 
 
     public static void main(String[] args) {
@@ -65,6 +66,7 @@ public class main {
         frmAltaOferta = new AltaOferta(ctrlUsuario);
         frmConsultaOferta = new ConsultaOferta(ctrlUsuario);
         frmPostulacionOferta = new PostulacionOferta(ctrlUsuario, frmConsultaOferta);
+        frmCrearPaqueteTiposPublicacion = new CrearPaqueteTiposPublicacion(ctrlTipos);
         frmAgregarTipoPublicacionAPaquete = new AgregarTipoPublicacionAPaquete(ctrlTipos);
         frmAgregarTipoPublicacionAPaquete.setBounds(0, 0, 423, 266);
         frmTrabajoUy.getContentPane().add(frmAgregarTipoPublicacionAPaquete);
@@ -72,9 +74,12 @@ public class main {
         frmTrabajoUy.getContentPane().add(frmAltaOferta);
         frmTrabajoUy.getContentPane().add(frmPostulacionOferta);
         frmTrabajoUy.getContentPane().add(frmConsultaOferta);
+        frmTrabajoUy.getContentPane().add(frmCrearPaqueteTiposPublicacion);
         frmConsultaUsuario = new ConsultaUsuario(ctrlUsuario, frmConsultaOferta);
         frmConsultaUsuario.setBounds(0,0, 500, 500);
         frmTrabajoUy.getContentPane().add(frmConsultaUsuario); 
+        frmConsultaPaquete = new ConsultaPaquete(ctrlTipos);
+        frmTrabajoUy.getContentPane().add(frmConsultaPaquete);
         
         frmTrabajoUy.getContentPane().setLayout(null);
         
@@ -160,6 +165,11 @@ public class main {
         menuTipos.add(menuItemAltaTipo);
         
         JMenuItem menuItemCrearPaquete = new JMenuItem("Crear Paquete de Tipos de Publicacion");
+        menuItemCrearPaquete.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		frmCrearPaqueteTiposPublicacion.setVisible(true);
+        	}
+        });
         menuTipos.add(menuItemCrearPaquete);
         
         JMenuItem menuItemAgregarTipo = new JMenuItem("Agregar Tipo de Publicacion a Paquete");
@@ -173,6 +183,12 @@ public class main {
         menuTipos.add(menuItemAgregarTipo);
         
         JMenuItem menuItemConsultaPaquete = new JMenuItem("Consulta de Paquete de Tipos de Publicacion");
+        menuItemConsultaPaquete.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                frmConsultaPaquete.cargarPaquetes();
+            	frmConsultaPaquete.setVisible(true);
+            }
+        });
         menuTipos.add(menuItemConsultaPaquete);
         
         JMenuItem menuItemAltaKeyword = new JMenuItem("Alta de Keyword");
