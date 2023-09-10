60a52d1cb6392dcd2956c890f5f509b74ea4e45f
Gaston Medeiros <gaston.medeiros@fing.edu.uy>
2023-09-10 14:34:55 -0300

    mas cambios en consulta usuario

--- ServidorWeb/src/main/webapp/js/consultaUsuario.js
+++ ServidorWeb/src/main/webapp/js/consultaUsuario.js
@@ -4,32 +4,22 @@ function mostrarOcultarDatos(){
     var blur= document.getElementById('popup');
     blur.classList.toggle('active');
 }
-function habilitaModificar(){
-    document.getElementById('txtNombre').disabled= false;
-    document.getElementById('txtApellido').disabled= false;
-    document.getElementById('passClave').disabled= false;
-    document.getElementById('dtFecha').disabled= false;
-    document.getElementById('txtNacionalidad').disabled= false;
-    document.getElementById('txtDescripcion').disabled= false;
-    document.getElementById('txtLink').disabled= false;
-    document.getElementById('txtImagen').disabled= false;
+function habilitaModificar(habilita){
+    document.getElementById('txtNombre').disabled= habilita;
+    document.getElementById('txtApellido').disabled= habilita;
+    document.getElementById('passClave').disabled= habilita;
+    document.getElementById('dtFecha').disabled= habilita;
+    document.getElementById('txtNacionalidad').disabled= habilita;
+    document.getElementById('txtDescripcion').disabled= habilita;
+    document.getElementById('txtLink').disabled= habilita;
+    document.getElementById('txtImagen').disabled= habilita;
 }
-function ocultarCampos(){
-    $('#divNac').css('visibility', 'visible');
-
-    if( $('#divNac').is(":visible") ) {
-        $('#divNac').css('display', 'none'); 
-      } else {
-        $('#divNac').css('display', 'block');
-      }
+function postulante(){
+    if(document.getElementById("empresa").style.display == "none"){
+        document.getElementById("empresa").style.display = "block";
+        document.getElementById("postulante").style.display = "none";
+    } else {
+        document.getElementById("empresa").style.display = "none";
+        document.getElementById("postulante").style.display = "block";
+    }
 }
-$('#mostrar').click(function(e) {
-  
-    $('#txtNacionalidad').css('visibility', 'visible');
-
-    if( $('#txtNacionalidad').is(":visible") ) {
-        $('#txtNacionalidad').css('display', 'none'); 
-      } else {
-        $('#txtNacionalidad').css('display', 'block');
-      }
-  });
