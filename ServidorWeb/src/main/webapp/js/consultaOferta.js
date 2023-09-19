a6d3c03707213ad28edefe5cb8cc75f64dfd9e08
Bruno Romero <brunnorag@gmail.com>
2023-09-19 20:06:53 -0300

    fix nav

--- ServidorWeb/src/main/webapp/js/consultaOferta.js
+++ ServidorWeb/src/main/webapp/js/consultaOferta.js
@@ -4,3 +4,9 @@ function mostrarOcultarDatos(){
     var blur= document.getElementById('popup');
     blur.classList.toggle('active');
 }
+
+document.getElementById("redForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    var selectedOption = document.getElementById("pageSelect").value;
+    window.location.href = selectedOption; // Redirect to the selected page
+});
